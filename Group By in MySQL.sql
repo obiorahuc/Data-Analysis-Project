47763a0e7ad511d45bd93d5e7050ffd30dc5c70e
Uchenna Obiorah <uc.obiorah@gmail.com>
2025-10-21 10:48:23 +0100

    Add files via upload

---
+++ Group By in MySQL.sql
@@ -0,0 +1,17 @@
+-- Group By
+
+SELECT * 
+FROM parks_and_recreation.employee_demographics
+;
+
+SELECT gender, AVG(age), MAX(age), MIN(age), COUNT(age)
+FROM parks_and_recreation.employee_demographics
+GROUP BY gender
+;
+
+-- ORDER BY
+
+SELECT *
+FROM parks_and_recreation.employee_demographics
+ORDER BY gender, age
+;
